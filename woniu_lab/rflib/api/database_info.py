3b4353db37c1c84cc8310eeadb3e297106932290
tanyy0724 <990724@qq.com>
2023-03-29 11:51:20 +0800

    框架

---
+++ woniu_lab/rflib/api/database_info.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# 2023/3/15 17:35
+
+database_info = dict(host="192.168.120.2",
+                     user="root",
+                     password="root",
+                     port=3307,
+                     database="woniu_lab"
+                     )
