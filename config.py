f9dd6553f14bdc6c7af0d0dc4624ab4d56f2ab66
woniuydz <1173010038@qq.com>
2023-03-30 20:40:02 +0800

    update

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+url = "http://localhost:8080"
+db_cfg = {"host": "192.168.74.1",
+          "port": 3306,
+          "user": "root",
+          "password": "123456",
+          "database": "woniuboss4.0"}
